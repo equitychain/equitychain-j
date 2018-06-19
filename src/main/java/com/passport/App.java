7212f739e6c655cf68fb8b6358afef8dc99243a5
AronWu <dev@inesv.com>
2018-06-20 05:05:34 +0800

    add console enable.

--- src/main/java/com/passport/App.java
+++ src/main/java/com/passport/App.java
@@ -1,17 +1,27 @@
 package com.passport;
 
 
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 
 
 /**
- * @author Aron wu BlockChain Passport implement Java
+ * @author Aron wu
+ * TODO: 2018/6/20  BlockChain Passport implement Java
  */
 @SpringBootApplication
 public class App {
 
   public static void main(String[] args) {
+    //enable console?
+    if(Arrays.stream(args).anyMatch("console"::equalsIgnoreCase)){
+      System.getProperties().setProperty("spring.shell.interactive.enabled","true");
+    }else{
+      System.getProperties().setProperty("spring.shell.interactive.enabled","false");
+    }
     SpringApplication.run(App.class, args);
   }
 }
---
+++ src/main/java/com/passport/conf/AppConfiguration.java
@@ -0,0 +1,13 @@
+package com.passport.conf;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Wu Created by SKINK on 2018/6/20.
+ */
+//@Configuration
+//@EnableAutoConfiguration(exclude = {org.springframework.shell.SpringShellAutoConfiguration.class,org.springframework.shell.jline.JLineShellAutoConfiguration.class})
+public class AppConfiguration {
+
+}
