cc99b0a4a806849541e91e8ffbcf89f0c0433bc4
garyinblack <310730596@qq.com>
2018-09-21 09:09:38 +0800

    使用dto封装返回的block

---
+++ src/main/java/com/passport/dto/coreobject/Block.java
@@ -0,0 +1,54 @@
+package com.passport.dto.coreobject;
+
+import java.util.List;
+
+/**
+ * @author Wu Created by SKINK on 2018/6/20.
+ */
+public class Block {
+  private Long blockSize;//区块大小
+  BlockHeader blockHeader;//区块头信息
+  private Integer transactionCount;//交易流水数量
+  List<Transaction> transactions;//交易流水
+  private Long blockHeight;
+
+  public Long getBlockSize() {
+    return blockSize;
+  }
+
+  public void setBlockSize(Long blockSize) {
+    this.blockSize = blockSize;
+  }
+
+  public BlockHeader getBlockHeader() {
+    return blockHeader;
+  }
+
+  public void setBlockHeader(BlockHeader blockHeader) {
+    this.blockHeader = blockHeader;
+  }
+
+  public Integer getTransactionCount() {
+    return transactionCount;
+  }
+
+  public void setTransactionCount(Integer transactionCount) {
+    this.transactionCount = transactionCount;
+  }
+
+  public List<Transaction> getTransactions() {
+    return transactions;
+  }
+
+  public void setTransactions(List<Transaction> transactions) {
+    this.transactions = transactions;
+  }
+
+  public Long getBlockHeight() {
+    return blockHeight;
+  }
+
+  public void setBlockHeight(Long blockHeight) {
+    this.blockHeight = blockHeight;
+  }
+}
