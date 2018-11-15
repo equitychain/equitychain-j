90f3471261001cc14f6e80e8c488b19ac4620365
root <919903326@qq.com>
2018-11-15 11:30:55 +0800

    test

--- src/main/java/com/passport/msghandler/AccountIpSyncRESP.java
+++ src/main/java/com/passport/msghandler/AccountIpSyncRESP.java
@@ -1,14 +1,15 @@
 package com.passport.msghandler;
 
 import com.google.common.base.Optional;
+import com.google.protobuf.ByteString;
 import com.passport.core.Account;
 import com.passport.db.dbhelper.BaseDBAccess;
 import com.passport.db.dbhelper.DBAccess;
-import com.passport.proto.AccountMessage;
-import com.passport.proto.NettyMessage;
+import com.passport.proto.*;
 import com.passport.utils.DataFormatUtil;
 import com.passport.utils.HttpUtils;
 import com.passport.utils.SerializeUtils;
+import com.passport.utils.StoryFileUtil;
 import io.netty.channel.ChannelHandlerContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,11 +18,14 @@ import java.math.BigDecimal;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Component("DATA_RESP_ACCOUNTIP_SYNC")
 public class AccountIpSyncRESP extends Strategy {
     @Autowired
     BaseDBAccess dbAccess;
+    @Autowired
+    StoryFileUtil storyFileUtil;
     @Override
     void handleMsg(ChannelHandlerContext ctx, NettyMessage.Message message) throws Exception {
         //收到消息进行处理
@@ -33,5 +37,18 @@ public class AccountIpSyncRESP extends Strategy {
             dbAccess.rocksDB.put( (clientIP+"_"+new String(account.getAddress().toByteArray())).getBytes(), SerializeUtils.serialize(new String(account.getAddress().toByteArray())));
             dbAccess.rocksDB.put( (new String(account.getAddress().toByteArray())+"_"+clientIP).getBytes(),SerializeUtils.serialize(new String(account.getAddress().toByteArray())));
         }
+        //发送本地账户
+        Set<String> localAddress = storyFileUtil.getAddresses();
+        NettyData.Data.Builder dataBuilder1 = NettyData.Data.newBuilder();
+        dataBuilder1.setDataType(DataTypeEnum.DataType.ACCOUNTIP_SYNC);
+        for (String address : localAddress) {
+            AccountMessage.Account.Builder builder1 = AccountMessage.Account.newBuilder();
+            builder1.setAddress(ByteString.copyFrom(address.getBytes()));
+            dataBuilder1.addAccounts(builder1.build());
+        }
+        NettyMessage.Message.Builder builder1 = NettyMessage.Message.newBuilder();
+        builder1.setData(dataBuilder1.build());
+        builder1.setMessageType(MessageTypeEnum.MessageType.DATA_RESP);
+        ctx.writeAndFlush(builder1.build());
     }
 }
