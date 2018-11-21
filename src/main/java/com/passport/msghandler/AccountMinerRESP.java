9591e60976eab4b854571ffe8da51b647881ef4f
hwh <13580127947@139.com>
2018-11-21 15:27:22 +0800

    测试

--- src/main/java/com/passport/msghandler/AccountMinerRESP.java
+++ src/main/java/com/passport/msghandler/AccountMinerRESP.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component("DATA_RESP_ACCOUNT_MINER")
@@ -26,18 +27,21 @@ public class AccountMinerRESP extends Strategy {
     StoryFileUtil storyFileUtil;
     @Override
     void handleMsg(ChannelHandlerContext ctx, NettyMessage.Message message) throws Exception {
-        //收到消息进行处理
-        logger.info("收到启动出块请求放入等待。到下个周期才允许启动");
+
         //获取节点的账户列表
         List<Trustee> trustees = dbAccess.listTrustees();
+        List<String> localaddress = new ArrayList<>();
         List<TrusteeMessage.Trustee> msgTrustees = message.getData().getTrusteeList();
         for(Trustee trustee : trustees){
             for(TrusteeMessage.Trustee trus:msgTrustees){
                 String address = new String(trus.getAddress().toByteArray());
                 if(address.equals(trustee.getAddress())){
                     SyncFlag.waitMiner.put(address,1);
+                    localaddress.add(address);
                 }
             }
         }
+        //收到消息进行处理
+        logger.info("收到启动出块请求放入等待"+localaddress+"到下个周期才允许启动");
     }
 }
