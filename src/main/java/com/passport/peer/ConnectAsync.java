76add49cef1df2b0f375b04bc1fc825a27817254
hwh <13580127947@139.com>
2018-12-17 09:51:08 +0800

    去掉心跳机制

--- src/main/java/com/passport/peer/ConnectAsync.java
+++ src/main/java/com/passport/peer/ConnectAsync.java
@@ -64,7 +64,7 @@ public class ConnectAsync {
                         protected void initChannel(SocketChannel sc) throws Exception {
                             ChannelPipeline pipeline = sc.pipeline();
                             //心跳检查 默认30s 现改成60s
-                            pipeline.addLast(new IdleStateHandler(60, 0, 0, TimeUnit.SECONDS));
+//                            pipeline.addLast(new IdleStateHandler(60, 0, 0, TimeUnit.SECONDS));
                             //将字节数组转换成Person对象和将Person对象转成字节数组,一共需要四个处理器
                             pipeline.addLast(new ProtobufVarint32FrameDecoder());
                             pipeline.addLast(new ProtobufDecoder(NettyMessage.Message.getDefaultInstance()));
@@ -110,7 +110,7 @@ public class ConnectAsync {
                         protected void initChannel(SocketChannel sc) throws Exception {
                             ChannelPipeline pipeline = sc.pipeline();
                             //心跳检测 默认30s 现改成60s
-                            pipeline.addLast(new IdleStateHandler(0,60,0, TimeUnit.SECONDS));
+//                            pipeline.addLast(new IdleStateHandler(0,60,0, TimeUnit.SECONDS));
                             //将字节数组转换成Person对象和将Person对象转成字节数组,一共需要四个处理器
                             pipeline.addLast(new ProtobufVarint32FrameDecoder());
                             pipeline.addLast(new ProtobufDecoder(NettyMessage.Message.getDefaultInstance()));
